b442769d0f930668a12ddf780feb9ab219ff186f
Thtsuman <tht.suman@gmail.com>
2019-11-23 22:10:54 +0530

    adding files

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './App.css';
+
+import Navbar from './components/navbar/Navbar.component';
+import Index from './components/index/Index.component';
+
+import { Provider } from './context'
+
+function App() {
+  return (
+    <Provider>
+      <Router>
+        <React.Fragment>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path='/' component={Index} />
+            </Switch>
+          </div>
+        </React.Fragment>
+      </Router>
+    </Provider>
+  );
+}
+
+export default App;
