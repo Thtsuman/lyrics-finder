ce622cad9a5d57b7aa6a35cf4b21adc5853fbe1a
Thtsuman <tht.suman@gmail.com>
2019-11-24 15:16:32 +0530

    adding lyrics page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css';
 
 import Navbar from './components/navbar/Navbar.component';
 import Index from './components/index/Index.component';
+import Lyrics from './components/lyrics/Lyrics.component';
 
 import { Provider } from './context'
 
@@ -16,6 +17,7 @@ function App() {
           <div className="container">
             <Switch>
               <Route exact path='/' component={Index} />
+              <Route exact path='/lyrics/track/:id' component={Lyrics} />
             </Switch>
           </div>
         </React.Fragment>
