1b6dd33d4785f8e1386e5da6d3f0d709fde3e72b
Thtsuman <tht.suman@gmail.com>
2019-11-24 15:15:41 +0530

    Updates

---
+++ precache-manifest.940d5dbc98388eca1b17ffe5fb60544d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99d594812d98ff1bb2668c66e9065699",
+    "url": "/lyrics-finder/index.html"
+  },
+  {
+    "revision": "01dce729f68e59ba12c5",
+    "url": "/lyrics-finder/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "d7b0eb66763ec68885d0",
+    "url": "/lyrics-finder/static/js/2.f00d3962.chunk.js"
+  },
+  {
+    "revision": "01dce729f68e59ba12c5",
+    "url": "/lyrics-finder/static/js/main.11e37d89.chunk.js"
+  },
+  {
+    "revision": "48d2460550283183996a",
+    "url": "/lyrics-finder/static/js/runtime-main.2996c753.js"
+  }
+]);
